c37f3498f9d3131d4775398738ec7bae87ad6c54
Christian <okekedev@outlook.com>
2025-08-15 13:57:00 -0500

    Updated Sependencies

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.alias = {
+  ...config.resolver.alias,
+  buffer: 'buffer',
+};
+
+config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
+
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = config;
